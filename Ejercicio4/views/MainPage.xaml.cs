07a962f2b68eb8672299afa00e298437e402ce44
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-12 18:35:48 +0100

    aa

---
+++ Ejercicio4/views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ejercicio4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
---
+++ Ejercicio4/vm/MainPageVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4.vm
+{
+    public class MainPageVM : INotifyPropertyChanged
+    {
+        private string nombre = "";
+        private string apellidos = "";
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                if (value != nombre)
+                {
+                    nombre = value;
+                    OnPropertyChanged();
+                    try
+                    {
+                        if (!string.IsNullOrEmpty(value) && value.EndsWith("n", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Apellidos = string.Empty;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception("Error");
+                    }
+                }
+            }
+        }
+
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+            }
+
+            set
+            {
+                if (value != apellidos)
+                {
+                    try
+                    {
+                        apellidos = value;
+                        OnPropertyChanged();
+                        if (!string.IsNullOrEmpty(value) && value.EndsWith("n", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Nombre = string.Empty;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception("Error");
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
